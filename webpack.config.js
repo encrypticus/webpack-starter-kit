ece40ef8d7eda75ce9febbcc104eb4a679d9994c
encrypticus <encrypticus@gmail.com>
2019-10-25 17:17:54 +0600

    Добавил плагин html-webpack-plugin.

--- webpack.config.js
+++ webpack.config.js
@@ -2,12 +2,15 @@ const webpack = require('webpack');
 const webpackMerge = require('webpack-merge'); // объединяет массивы и объекты конфигураций из нескольких файлов-модулей
 const setEntry = require('./webpack/options/entry'); // опция устанавливает точку входа для сборщика
 const setOutput = require('./webpack/options/output'); // опция устанавливает путь, по которому будет лежать бандл
+const htmlWebpackPlugin = require('./webpack/plugins/htmlWebpackPlugin'); // плагин генерирует html-файл в папке сборки
 
 module.exports = (env, args) => {
-  const commonConfig = {
-    entry: setEntry(),
-    output: setOutput()
-  };
+  const commonConfig = webpackMerge({
+      entry: setEntry(),
+      output: setOutput(),
+    },
+    htmlWebpackPlugin()
+  );
 
   return webpackMerge(
     commonConfig
