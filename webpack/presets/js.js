69c965fdf7e19b99619e708fe99a3a7d387785eb
encrypticus <encrypticus@gmail.com>
2019-11-09 21:45:06 +0600

    Добавил eslint.

--- webpack/presets/js.js
+++ webpack/presets/js.js
@@ -1,9 +1,15 @@
 const babelLoader = require('../loaders/babel-loader');
+const eslintLoader = require('../loaders/eslint-loader');
 
 // Объект настроек по умолчанию
 defaultOptions = {
   babelLoader: {
     configFile: require('path').resolve(__dirname, '../configs/babel.config.js')
+  },
+  eslintLoader: {
+    emitWarning: true,
+    configFile: require('path').resolve(__dirname, '../configs/.eslintrc.js'),
+    fix: true
   },
   exclude: /node_modules/
 };
@@ -13,8 +19,22 @@ defaultOptions = {
  * сборщик будет обрабатывать файлы с тем или иным расширением (модули), а также указываются лоадеры,
  * которые будут обрабатывать эти файлы.
  * babelLoader - это объект настроек для лоадера babel-loader
+ * eslintLoader - это объект настроек для лоадера eslint-loader
  * @param {Object} options настройки для пресета
  * @returns {Object} свойство объекта конфига сборщика, пресет для js-файлов
+ * @example
+ * const processJs = require('./webpack/presets/js');
+ * // вызов с настройками по умолчанию
+ * processJs();
+ * //вызов со своими настройками
+ * processJs({
+ *  eslintLoader: {
+ *    configFile: require('path').resolve(__dirname, '../../.eslintrc.js'),
+ *    cache: true,
+ *    formatter: require('eslint-friendly-formatter')
+ *  },
+ *  exclude: /node_modules/
+ * })
  */
 module.exports = (options = defaultOptions) => {
   return {
@@ -23,7 +43,10 @@ module.exports = (options = defaultOptions) => {
         {
           test: /\.js$/,
           exclude: options.exclude,
-          use: babelLoader(options.babelLoader)
+          use: [
+            babelLoader(options.babelLoader),
+            eslintLoader(options.eslintLoader)
+          ]
         }
       ]
     }
