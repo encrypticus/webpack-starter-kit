848be2ec9ca46f6fc916f84efd935b113eff4463
encrypticus <encrypticus@gmail.com>
2021-01-29 15:31:06 +0600

    Component `main-nav`: рефакторинг.

--- src/components/main-nav/main-nav.js
+++ src/components/main-nav/main-nav.js
@@ -1,68 +1,30 @@
 import './main-nav.scss';
+import MetisMenu from 'metismenujs';
+import OnOffCanvas from 'onoffcanvas';
+import 'metismenujs/scss/metismenujs.scss';
+import 'onoffcanvas/scss/onoffcanvas.scss';
 
-const mainList = document.querySelector('.main-nav__list_level-1'); // главный список меню верхнего уровня
-const sublists = document.querySelectorAll('.main-nav__item_has-child > .main-nav__link + .main-nav__list'); // все вложенные списки
-const { body } = document;
-const { documentElement } = document;
-const { forEach } = Array.prototype;
-let zIndex = 2;
+const mainNav = new MetisMenu('.main-nav_full .main-nav__list_level-1');
+const mainNavMob = new MetisMenu('.main-nav_mobile .main-nav__list_level-1');
 
-function createMenu(list) {
-  const listItems = list.children;
+const mobMenuCanvas = document.querySelector('#mob-menu-canvas');
+const hamburger = document.querySelector('.hamburger');
+const mainNavMobCanvas = new OnOffCanvas(mobMenuCanvas);
 
-  if (listItems.length > 0) {
-    forEach.call(listItems, (listItem) => {
-      if (listItem.classList.contains('main-nav__item_has-child')) {
-        const childLink = listItem.children[0]; // ссылка текущего элемента списка
-        const childList = listItem.children[1]; // вложенный список текущего элемента списка
-        const parentList = listItem.parentElement; // родительский список текущего элемента списка
-        const siblings = parentList.children; // соседние элементы списка текущего элемента списка
-        const childListSublists = childList.querySelectorAll('.main-nav__list'); // все дочерние списки любой глубины вложенности дочернего списка текущего элемента списка
+mainNavMobCanvas.on('show.onoffcanvas', () => {
+  hamburger.classList.add('is-active');
+});
 
-        childList.style.zIndex = ++zIndex;
+mainNavMobCanvas.on('hide.onoffcanvas', () => {
+  hamburger.classList.remove('is-active');
+});
 
-        childLink.addEventListener('click', (event) => {
-          event.preventDefault();
-          event.stopPropagation();
+hamburger.addEventListener('click', () => {
+  mobMenuCanvas.classList.add('onoffcanvas_opacity');
+}, false);
 
-          // Закрыть все вложенные списки
-          forEach.call(childListSublists, (childListSublist) => {
-            childListSublist.classList.remove('main-nav__list_visible');
-          });
-
-          // Закрыть все вложенные списки соседних элементов списка
-          forEach.call(siblings, (sibling) => {
-            if (sibling !== listItem) {
-              const siblingsLists = sibling.querySelectorAll('.main-nav__list');
-
-              forEach.call(siblingsLists, (siblingList) => {
-                siblingList.classList.remove('main-nav__list_visible');
-              });
-            }
-          });
-
-          // Скрыть/показать вложенный список
-          childList.classList.toggle('main-nav__list_visible');
-          // childLink.classList.toggle('main-nav__link_active');
-        });
-
-        createMenu(childList);
-      }
-    });
+window.addEventListener('resize', () => {
+  if (window.innerWidth > 767) {
+    mainNavMobCanvas.hide();
   }
-}
-
-// При клике на body и на html нужно закрыть все вложенные списки
-documentElement.addEventListener('click', () => {
-  forEach.call(sublists, (sublist) => {
-    sublist.classList.remove('main-nav__list_visible');
-  });
 });
-
-body.addEventListener('click', () => {
-  forEach.call(sublists, (sublist) => {
-    sublist.classList.remove('main-nav__list_visible');
-  });
-});
-
-createMenu(mainList);
